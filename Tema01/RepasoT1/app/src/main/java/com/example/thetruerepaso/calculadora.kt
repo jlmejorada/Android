d6cc428a89f7263d21619edb6db29dee9b1e4572
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-10-21 12:48:59 +0200

    Proyectos Android

---
+++ Tema01/RepasoT1/app/src/main/java/com/example/thetruerepaso/calculadora.kt
@@ -0,0 +1,14 @@
+package com.example.thetruerepaso
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun calculadoraVista(navController: NavHostController){
+    Column {
+        Text("Hola")
+
+    }
+}
---
+++ Tema01/RepasoT1/app/src/main/java/com/example/thetruerepaso/menuCarga.kt
@@ -0,0 +1,82 @@
+package com.example.thetruerepaso
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun pantallaDeCarga(navController: NavHostController) {
+    var nombre by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable {  mutableStateOf("") }
+    Column(
+        Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row(
+            Modifier
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Usuario",
+                modifier = Modifier
+                    .width(100.dp)
+                    .padding(end=20.dp),
+                textAlign = TextAlign.End
+            )
+            TextField(
+                value=nombre,
+                modifier = Modifier
+                    .width(200.dp),
+                onValueChange = {nombre=it}
+            )
+        }
+        Row(
+            Modifier
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Contraseña",
+                Modifier
+                    .width(100.dp)
+                    .padding(end=20.dp),
+                textAlign = TextAlign.End
+            )
+            TextField(
+                value=password,
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .width(200.dp),
+                onValueChange = {password=it}
+            )
+        }
+        Button(
+            onClick = {
+                (navController.navigate("calculadora"))
+            }
+        ) {
+            Text("Entrar")
+        }
+    }
+}
