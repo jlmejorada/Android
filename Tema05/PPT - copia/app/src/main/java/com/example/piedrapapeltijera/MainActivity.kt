38ed2dc53d25e7e31823aac320fb44e322afa6e0
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-11-12 13:07:39 +0100

    UPD PTT con BDD

---
+++ Tema05/PPT - copia/app/src/main/java/com/example/piedrapapeltijera/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.piedrapapeltijera
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.example.piedrapapeltijera.ui.theme.PiedraPapelTijeraTheme
+import kotlinx.coroutines.CoroutineScope
+
+class MainActivity : ComponentActivity() {
+    companion object {
+        lateinit var coroutine: CoroutineScope
+        lateinit var basedatos: JugadorDataBase
+        lateinit var todas: List<JugadorEntity>
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        basedatos = Room.databaseBuilder(
+            this, JugadorDataBase::class.java, "tareas-db"
+        ).build()
+        enableEdgeToEdge()
+        setContent {
+            PiedraPapelTijeraTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "pantalla1"
+                ) {
+                    composable("pantalla1") { PantallaInicio(navController) }
+                    composable("pantalla2") { PantallaJuego(navController) }
+                }
+            }
+        }
+    }
+}
